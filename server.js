423d7980a89eefb8a95f8621e2e05d529e7a6980
DESKTOP-O1J837U\talbo <talboosy@gmail.com>
2019-01-02 14:46:28 +0200

    Added projects page

--- server.js
+++ server.js
@@ -40,14 +40,20 @@ app.get('/', (req, res)=>{
     res.render('home.hbs',{
         pageTitle: 'Welcome to my Node web',
         welcomeMessage: 'Tal Boosy Creator of the page',
-    })
-})
+    });
+});
 
 app.get('/about',(req,res)=>{
     res.render('about.hbs',{
         pageTitle:'About Page',
     });
-})
+});
+
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle:'Projects Page'
+    });
+});
 
 app.get('/bad',(req, res)=>{
     res.send({
